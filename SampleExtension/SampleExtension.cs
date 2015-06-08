e6bafae235fd172be9cbcf1dfaaa0fff0701614b
mgonzalez <mgonzalez@.(none)>
2015-06-08 17:17:30 +0200

    Implemented the sample extension using the new architecture

--- SampleExtension/SampleExtension.cs
+++ SampleExtension/SampleExtension.cs
@@ -1,83 +1,193 @@
-﻿using log4net;
-using Newtonsoft.Json;
-using SampleExtension.Model;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
-using System.Text;
 
-namespace Codice.Client.Extension
+using log4net;
+using Newtonsoft.Json;
+
+using Codice.Client.IssueTracker.SampleExtension.Model;
+
+
+namespace Codice.Client.IssueTracker.SampleExtension
 {
-    public class SampleExtension : BasePlasticExtension
+    public class SampleExtension : IPlasticIssueTrackerExtension
     {
-        protected BaseExtensionConfiguration mConfig;
-        private static readonly ILog mLog = LogManager.GetLogger("extensions");
+        internal const string BRANCH_PREFIX_KEY = "Branch prefix";
+        internal const string USER_KEY = "User ID";
 
-        public SampleExtension()
+        const string POST_URL = "http://jsonplaceholder.typicode.com/posts/{0}";
+        const string ALL_POSTS_URL = "http://jsonplaceholder.typicode.com/posts";
+        const string POSTS_BY_USER_URL = "http://jsonplaceholder.typicode.com/posts?userId={0}";
+
+        IssueTrackerConfiguration mConfig;
+
+        static readonly ILog mLog = LogManager.GetLogger("sampleextension");
+
+        internal SampleExtension(IssueTrackerConfiguration config)
         {
-            mConfig = new BaseExtensionConfiguration();
-            mConfig.BranchPrefix = "sample";
-            mConfig.SetDefaultAttributePrefix("sample");
-            mConfig.WorkingMode = ExtensionWorkingMode.TaskOnChangeset;
-            mBaseConfig = mConfig;
+            mConfig = config;
 
-            mLog.Info("Extension initialized");
+            mLog.Info("Sample issue tracker is initialized");
         }
 
-        public override string GetName()
+        public void Connect()
+        {
+            // No action needed
+        }
+
+        public void Disconnect()
+        {
+            // No action needed
+        }
+
+        public string GetExtensionName()
         {
             return "My awesome extension";
         }
 
-        public override PlasticTask[] LoadTask(string[] id, string repName)
+        public List<PlasticTask> GetPendingTasks(string assignee)
         {
-            PlasticTask[] tasks = new PlasticTask[id.Length];
+            int assigneeId;
+            if (string.IsNullOrEmpty(assignee) || !int.TryParse(assignee, out assigneeId))
+                return new List<PlasticTask>();
 
-            for (int i = 0; i < id.Length; i++)
+            return QueryServiceForTasks(string.Format(POSTS_BY_USER_URL, assignee));
+        }
+
+        public List<PlasticTask> GetPendingTasks()
+        {
+            return QueryServiceForTasks(ALL_POSTS_URL);
+        }
+
+        public PlasticTask GetTaskForBranch(string fullBranchName)
+        {
+            return LoadSingleTask(GetTaskIdFromBranchName(GetBranchName(fullBranchName)));
+        }
+
+        public Dictionary<string, PlasticTask> GetTasksForBranches(List<string> fullBranchNames)
+        {
+            Dictionary<string, PlasticTask> result = new Dictionary<string, PlasticTask>();
+            foreach(string fullBranchName in fullBranchNames)
             {
-                if (string.IsNullOrEmpty(id[i]))
-                {
+                string taskId = GetTaskIdFromBranchName(GetBranchName(fullBranchName));
+                result.Add(fullBranchName, LoadSingleTask(taskId));
+            }
+            return result;
+        }
+
+        public List<PlasticTask> LoadTasks(List<string> taskIds)
+        {
+            List<PlasticTask> result = new List<PlasticTask>();
+
+            foreach (string taskId in taskIds)
+            {
+                PlasticTask loadedTask = LoadSingleTask(taskId);
+                if (loadedTask == null)
                     continue;
-                }
-
-                string uri = string.Format("http://jsonplaceholder.typicode.com/posts/{0}", id[i]);
-
-                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(uri);
-                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
-
-                using (StreamReader reader = new StreamReader(resp.GetResponseStream()))
-                {
-                    string resultJson = reader.ReadToEnd();
-                    var myServiceDataForTask = JsonConvert.DeserializeObject<MyServiceData>(resultJson);
-                    tasks[i] = new PlasticTask()
-                    {
-                        Id = myServiceDataForTask.Id.ToString(),
-                        Owner = myServiceDataForTask.UserId.ToString(),
-                        RepName = repName,
-                        Title = myServiceDataForTask.Title,
-                        Description = myServiceDataForTask.Body,
-                        Status = "working"
-                    };
-                }
-
+                result.Add(loadedTask);
             }
 
-            return tasks;
+            return result;
         }
 
-        public override void OpenTask(string id, string repName)
+        public void LogCheckinResult(PlasticChangeset changeset, List<PlasticTask> tasks)
         {
-            Process.Start(string.Format("https://www.google.es/search?q={0}+{1}", id, repName));
+            // Not supported
         }
 
-        public override PlasticTaskConfiguration[] GetTaskConfiguration(string task)
+        public void MarkTaskAsOpen(string taskId, string assignee)
         {
-            PlasticTaskConfiguration taskConf = new PlasticTaskConfiguration();
-            taskConf.TaskId = long.Parse(task);
-            taskConf.PlasticPrefix = "sample";
-            return new PlasticTaskConfiguration[] { taskConf };
+            // Not supported
+        }
+
+        public void OpenTaskExternally(string taskId)
+        {
+            Process.Start(string.Format(
+                "https://www.google.es/search?q={0}", taskId));
+        }
+
+        public bool TestConnection(IssueTrackerConfiguration configuration)
+        {
+            return true;
+        }
+
+        public void UpdateLinkedTasksToChangeset(PlasticChangeset changeset, List<string> tasks)
+        {
+            // Not supported
+        }
+
+        PlasticTask LoadSingleTask(string taskId)
+        {
+            if (string.IsNullOrEmpty(taskId))
+                return null;
+
+            string uri = string.Format(POST_URL, taskId);
+            string resultJson = PerformJsonRequest(uri);
+
+            return BuildTaskFromJson(
+                JsonConvert.DeserializeObject<MyServiceData>(resultJson));
+        }
+
+        string GetBranchName(string fullBranchName)
+        {
+            int lastSeparatorIndex = fullBranchName.LastIndexOf('/');
+
+            if (lastSeparatorIndex < 0 )
+                return fullBranchName;
+
+            if (lastSeparatorIndex == fullBranchName.Length - 1)
+                return string.Empty;
+
+            return fullBranchName.Substring(lastSeparatorIndex + 1);
+        }
+
+        string GetTaskIdFromBranchName(string branchName)
+        {
+            string prefix = mConfig.GetValue(BRANCH_PREFIX_KEY);
+            if (string.IsNullOrEmpty(prefix))
+                return branchName;
+
+            if (!branchName.StartsWith(branchName) || branchName == prefix)
+                return string.Empty;
+
+            return branchName.Substring(prefix.Length);
+        }
+
+        List<PlasticTask> QueryServiceForTasks(string uri)
+        {
+            List<PlasticTask> result = new List<PlasticTask>();
+            string jsonResult = PerformJsonRequest(uri);
+
+            MyServiceData[] deserializedServiceData =
+                JsonConvert.DeserializeObject<MyServiceData[]>(jsonResult);
+
+            foreach (MyServiceData serviceData in deserializedServiceData)
+                result.Add(BuildTaskFromJson(serviceData));
+            return result;
+        }
+
+        string PerformJsonRequest(string targetUri)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(targetUri);
+
+            using (HttpWebResponse resp = (HttpWebResponse)req.GetResponse())
+            using (StreamReader reader = new StreamReader(resp.GetResponseStream()))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        PlasticTask BuildTaskFromJson(MyServiceData jsonData)
+        {
+            return new PlasticTask()
+            {
+                Id = jsonData.Id.ToString(),
+                Owner = jsonData.UserId.ToString(),
+                Title = jsonData.Title,
+                Description = jsonData.Body,
+                Status = "working"
+            };
         }
     }
 }
---
+++ SampleExtension/SampleExtensionFactory.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codice.Client.IssueTracker.SampleExtension
+{
+    public class SampleExtensionFactory : IPlasticIssueTrackerExtensionFactory
+    {
+        public IssueTrackerConfiguration GetConfiguration(
+            IssueTrackerConfiguration storedConfiguration)
+        {
+            List<IssueTrackerConfigurationParameter> parameters
+                = new List<IssueTrackerConfigurationParameter>();
+
+            ExtensionWorkingMode workingMode = storedConfiguration.WorkingMode;
+            if (workingMode == ExtensionWorkingMode.None)
+                workingMode = ExtensionWorkingMode.TaskOnBranch;
+
+            string user = storedConfiguration.GetValue(SampleExtension.USER_KEY);
+            if (string.IsNullOrEmpty(user))
+                user = "1";
+
+            string prefix = storedConfiguration.GetValue(SampleExtension.BRANCH_PREFIX_KEY);
+            if (string.IsNullOrEmpty(prefix))
+                prefix = "scm";
+
+            IssueTrackerConfigurationParameter userIdParam =
+                new IssueTrackerConfigurationParameter()
+            {
+                Name = SampleExtension.USER_KEY,
+                Value = user,
+                Type = IssueTrackerConfigurationParameterType.User,
+                IsGlobal = false
+            };
+            
+            IssueTrackerConfigurationParameter branchPrefixParam =
+                new IssueTrackerConfigurationParameter()
+            {
+                Name = SampleExtension.USER_KEY,
+                Value = storedConfiguration.GetValue(SampleExtension.USER_KEY),
+                Type = IssueTrackerConfigurationParameterType.User,
+                IsGlobal = false
+            };
+
+            return new IssueTrackerConfiguration(workingMode, parameters);
+        }
+
+        public IPlasticIssueTrackerExtension GetIssueTrackerExtension(
+            IssueTrackerConfiguration configuration)
+        {
+            return new SampleExtension(configuration);
+        }
+
+        public string GetIssueTrackerName()
+        {
+            return "Sample extension";
+        }
+    }
+}
